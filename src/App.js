f810b0e2016a6d35ae6e88b42cfbf652aa21e79c
Benjamin-Pontoizeau <benjamin.pontoizeau@gmail.com>
2022-04-25 16:25:30 +0200

    lecture de la météo pour une ville donnée et affichage de l'icone du temps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MeteoWidget from './MeteoWidget';
 
-function App() {
+function App() {  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MeteoWidget city="Montpellier" code={34000}/>
     </div>
   );
 }
